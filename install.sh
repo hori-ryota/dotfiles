900403eeb5aa43a39df7db5a7649c17569825a7e
Hori Ryota <hori.ryota@gmail.com>
2015-02-06 17:55:59 +0900

    Modify install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
-BASEDIR=$HOME/.dotfiles
+#!/bin/sh
+
+BASEDIR="$HOME"/.dotfiles
 ZDOTDIR=$BASEDIR/zsh
 VDOTDIR=$BASEDIR/vim
 
 # zsh with prezto
-ln -s $BASEDIR/zprezto $HOME/.zprezto
-ln -s $ZDOTDIR/zprezto/runcoms/zlogin $HOME/.zlogin
-ln -s $ZDOTDIR/zprezto/runcoms/zlogout $HOME/.zlogout
-ln -s $ZDOTDIR/zprezto/runcoms/zpreztorc $HOME/.zpreztorc
-ln -s $ZDOTDIR/zprezto/runcoms/zprofile $HOME/.zprofile
-ln -s $ZDOTDIR/zprezto/runcoms/zshenv $HOME/.zshenv
-ln -s $ZDOTDIR/zprezto/runcoms/zshrc $HOME/.zshrc
+ln -ins "$BASEDIR"/zprezto "$HOME"/.zprezto
+ln -ins "$ZDOTDIR"/zprezto/runcoms/zlogin "$HOME"/.zlogin
+ln -ins "$ZDOTDIR"/zprezto/runcoms/zlogout "$HOME"/.zlogout
+ln -ins "$ZDOTDIR"/zprezto/runcoms/zpreztorc "$HOME"/.zpreztorc
+ln -ins "$ZDOTDIR"/zprezto/runcoms/zprofile "$HOME"/.zprofile
+ln -ins "$ZDOTDIR"/zprezto/runcoms/zshenv "$HOME"/.zshenv
+ln -ins "$ZDOTDIR"/zprezto/runcoms/zshrc "$HOME"/.zshrc
 
 # vim
-ln -s $BASEDIR/vim $HOME/.vim
-ln -s $VDOTDIR/vimrc $HOME/.vimrc
+ln -ins "$BASEDIR"/vim "$HOME"/.vim
+ln -ins "$VDOTDIR"/vimrc "$HOME"/.vimrc
 
 # tmux
-ln -s $BASEDIR/tmux.conf $HOME/.tmux.conf
+ln -ins "$BASEDIR"/tmux.conf "$HOME"/.tmux.conf
 
 # git
-ln -s $BASEDIR/gitconfig $HOME/.gitconfig
-ln -s $BASEDIR/gitignore $HOME/.gitignore_global
+ln -ins "$BASEDIR"/gitconfig "$HOME"/.gitconfig
+ln -ins "$BASEDIR"/gitignore "$HOME"/.gitignore_global
