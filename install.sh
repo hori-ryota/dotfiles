f87cb77b0f498f1abf668b0fc526b3e2355be2e3
Hori Ryota <hori.ryota@gmail.com>
2015-02-06 13:37:10 +0900

    Fixed bugs of install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 BASEDIR=$HOME/.dotfiles
-ZDOTDIR=$BASEDIR/zsh/prezto
+ZDOTDIR=$BASEDIR/zsh
+VDOTDIR=$BASEDIR/vim
 
 # zsh with prezto
-ln -s $BASEDIR/zprezto
+ln -s $BASEDIR/zprezto $HOME/.zprezto
 ln -s $ZDOTDIR/zprezto/runcoms/zlogin $HOME/.zlogin
 ln -s $ZDOTDIR/zprezto/runcoms/zlogout $HOME/.zlogout
 ln -s $ZDOTDIR/zprezto/runcoms/zpreztorc $HOME/.zpreztorc
 ln -s $ZDOTDIR/zprezto/runcoms/zprofile $HOME/.zprofile
 ln -s $ZDOTDIR/zprezto/runcoms/zshenv $HOME/.zshenv
 ln -s $ZDOTDIR/zprezto/runcoms/zshrc $HOME/.zshrc
+
+# vim
+ln -s $BASEDIR/vim $HOME/.vim
+ln -s $VDOTDIR/vimrc $HOME/.vimrc
 
 # tmux
 ln -s $BASEDIR/tmux.conf $HOME/.tmux.conf
