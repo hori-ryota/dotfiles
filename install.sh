5e5d57274f5359bd3e22312fa6f14d8956e0eba8
Hori Ryota <hori.ryota@gmail.com>
2015-02-17 09:43:43 +0900

    Add changing default shell to install.sh for zsh

--- install.sh
+++ install.sh
@@ -23,3 +23,9 @@ ln -ins "$BASEDIR"/tmux.conf "$HOME"/.tmux.conf
 # git
 ln -ins "$BASEDIR"/gitconfig "$HOME"/.gitconfig
 ln -ins "$BASEDIR"/gitignore "$HOME"/.gitignore_global
+
+# change default shell to zsh (installed by homebrew)
+if ! grep /usr/local/bin/zsh /etc/shells > /dev/null 2>&1; then
+  sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
+  chsh -s /usr/local/bin/zsh
+fi
