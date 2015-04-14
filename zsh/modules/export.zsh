b2ae1d444f68353bea18cacc2a9b900ca86efb7d
Hori Ryota <hori.ryota@gmail.com>
2015-04-14 11:48:41 +0900

    Modify exports file

--- zsh/modules/export.zsh
+++ zsh/modules/export.zsh
@@ -1,9 +1,14 @@
 export HISTTIMEFORMAT=' %F %T ' # Historyファイルのフォーマット
 export PAGER="/usr/bin/less -R"
 
-#Homebrewのための設定
+# Homebrewのための設定
 export PATH=/usr/local/bin:$PATH
 export PATH=${HOME}/local/bin:$PATH
+# For brew cask
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 # nodebrew
 export PATH=$HOME/.nodebrew/current/bin:$PATH
+
+# tmuxinator
+export EDITOR='vim'
