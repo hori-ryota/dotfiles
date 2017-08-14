f5288b1a84b2f294680aa16d754fc797198f82cf
hori-ryota <hori_ryota@cyberagent.co.jp>
2017-08-14 10:38:05 +0900

    Add script for global npm

---
+++ install/npm-g.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# for Talkie
+npm install -g resumify
+
+# typescript
+npm install -g typescript
+npm install -g typescript-tools
+
+# formatter
+npm install -g js-beautify
+npm install -g typescript-formatter
