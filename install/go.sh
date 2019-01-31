7247e76a9e9dbec39e376523bc592a2d36d8b1d9
hori-ryota <hori.ryota@gmail.com>
2019-01-31 16:37:03 +0900

    update install script for go

--- install/go.sh
+++ install/go.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo 'Install github.com/mattn/files'
-go get github.com/mattn/files
+go get -u -v github.com/mattn/files
 echo 'Install github.com/rakyll/hey'
-go get github.com/rakyll/hey
+go get -u -v github.com/rakyll/hey
 echo 'Install github.com/saibing/bingo'
-go get github.com/saibing/bingo
+go get -u -v github.com/saibing/bingo
 echo 'Install github.com/golangci/golangci-lint'
-go get github.com/golangci/golangci-lint/cmd/golangci-lint
+go get -u -v github.com/golangci/golangci-lint/cmd/golangci-lint
