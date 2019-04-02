1ea3573f4e98276a11651d918c48227bacb2ab29
hori-ryota <hori.ryota@gmail.com>
2019-04-02 15:43:57 +0900

    replace bingo to gopls

--- install/go.sh
+++ install/go.sh
@@ -4,7 +4,7 @@ echo 'Install github.com/mattn/files'
 go get -u -v github.com/mattn/files
 echo 'Install github.com/rakyll/hey'
 go get -u -v github.com/rakyll/hey
-echo 'Install github.com/saibing/bingo'
-go get -u -v github.com/saibing/bingo
+echo 'Install golang.org/x/tools/cmd/gopls'
+go get -u -v golang.org/x/tools/cmd/gopls
 echo 'Install github.com/golangci/golangci-lint'
 go get -u -v github.com/golangci/golangci-lint/cmd/golangci-lint
