a1860e85b76342ebac4929dd451d346cdf353e73
hori-ryota <hori.ryota@gmail.com>
2019-04-21 10:13:57 +0900

    update

--- install/go.sh
+++ install/go.sh
@@ -8,3 +8,5 @@ echo 'Install golang.org/x/tools/cmd/gopls'
 go get -u -v golang.org/x/tools/cmd/gopls
 echo 'Install github.com/golangci/golangci-lint'
 go get -u -v github.com/golangci/golangci-lint/cmd/golangci-lint
+echo 'Install github.com/golang/dep/cmd/dep'
+go get -u -v github.com/golang/dep/cmd/dep
