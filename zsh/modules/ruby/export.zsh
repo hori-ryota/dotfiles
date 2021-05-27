7d6ff26dbf9a97878d9215a417a3299d7226f0da
hori-ryota <hori.ryota@gmail.com>
2021-05-27 10:09:42 +0900

    add ruby

---
+++ zsh/modules/ruby/export.zsh
@@ -0,0 +1,1 @@
+export RUBY_VERSION=3.0.1
---
+++ zsh/modules/ruby/install.sh
@@ -0,0 +1,7 @@
+source "$(dirname "${BASH_SOURCE:-$0}")/../0-base-envs/export.zsh"
+zsh -c "$(dirname "${BASH_SOURCE:-$0}")/../1-asdf/install.sh"
+source "$(dirname "${BASH_SOURCE:-$0}")/export.zsh"
+
+asdf plugin-add ruby
+asdf install ruby $RUBY_VERSION
+asdf global ruby $RUBY_VERSION
