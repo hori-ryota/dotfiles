667fbe7fb23e561010b75d1375732dd3eb8d3744
hori-ryota <hori.ryota@gmail.com>
2021-12-20 13:09:08 +0900

    move gitignore to XDG_CONFIG_HOME

--- zsh/modules/version_controll_systems/install.sh
+++ zsh/modules/version_controll_systems/install.sh
@@ -6,5 +6,7 @@ brew install git-lfs
 
 source "$(dirname "${BASH_SOURCE:-$0}")/../0-base-envs/export.zsh"
 
+mkdir -p "$XDG_CONFIG_HOME/git"
+
 ln -snf "$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)/gitconfig" "$HOME/.gitconfig"
-ln -snf "$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)/gitignore" "$HOME/.gitignore_global"
+ln -snf "$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)/gitignore" "$XDG_CONFIG_HOME/git/ignore"
