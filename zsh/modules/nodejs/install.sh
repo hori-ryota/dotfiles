5491d8ef55fa50605165074c2ae2d2f1dfd7312c
hori-ryota <hori.ryota@gmail.com>
2022-08-14 16:44:15 +0900

    fix bug

--- zsh/modules/nodejs/install.sh
+++ zsh/modules/nodejs/install.sh
@@ -3,7 +3,6 @@ zsh -c "$(dirname "${BASH_SOURCE:-$0}")/../1-asdf/install.sh"
 source "$(dirname "${BASH_SOURCE:-$0}")/export.zsh"
 
 asdf plugin-add nodejs
-zsh -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
 asdf install nodejs $NODE_VERSION
 asdf global nodejs $NODE_VERSION
 
