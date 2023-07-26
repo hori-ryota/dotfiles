4af166d0feee0185890c80dd216670ec2d7167f4
hori-ryota <hori.ryota@gmail.com>
2023-07-26 22:54:49 +0900

    bump tools

--- zsh/modules/deno/export.zsh
+++ zsh/modules/deno/export.zsh
@@ -1,1 +1,1 @@
-export DENO_VERSION=1.35.1
+export DENO_VERSION=1.35.2
--- zsh/modules/nodejs/export.zsh
+++ zsh/modules/nodejs/export.zsh
@@ -1,4 +1,4 @@
-export NODE_VERSION=18.16.1
+export NODE_VERSION=18.17.0
 
 export PNPM_HOME="$XDG_DATA_HOME/.local/share/pnpm"
 case ":$PATH:" in
