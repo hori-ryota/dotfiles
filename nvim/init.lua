d1371f26230587f64f9612493d1c1446e6c96e4c
hori-ryota <hori.ryota@gmail.com>
2023-10-22 11:34:01 +0900

    introduce treesitter-indent-object

--- nvim/init.lua
+++ nvim/init.lua
@@ -1584,6 +1584,35 @@ require('lazy').setup({
     end,
   },
   {
+    "kiyoon/treesitter-indent-object.nvim",
+    keys = {
+      {
+        "ai",
+        function() require 'treesitter_indent_object.textobj'.select_indent_outer() end,
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (outer)",
+      },
+      {
+        "aI",
+        function() require 'treesitter_indent_object.textobj'.select_indent_outer(true) end,
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (outer, line-wise)",
+      },
+      {
+        "ii",
+        function() require 'treesitter_indent_object.textobj'.select_indent_inner() end,
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (inner, partial range)",
+      },
+      {
+        "iI",
+        function() require 'treesitter_indent_object.textobj'.select_indent_inner(true, 'V') end,
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (inner, entire range) in line-wise visual mode",
+      },
+    },
+  },
+  {
     -- comment
     'numToStr/Comment.nvim',
     keys = {
