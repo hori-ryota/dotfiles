e72c8b9948bc6450bac6b7ec8dfdf1a39c343899
hori-ryota <hori.ryota@gmail.com>
2024-03-20 16:06:51 +0900

    bump libs

--- zsh/modules/deno/export.zsh
+++ zsh/modules/deno/export.zsh
@@ -1,1 +1,1 @@
-export DENO_VERSION=1.40.4
+export DENO_VERSION=1.41.3
--- zsh/modules/nodejs/export.zsh
+++ zsh/modules/nodejs/export.zsh
@@ -1,4 +1,4 @@
-export NODE_VERSION=20.11.0
+export NODE_VERSION=20.11.1
 
 export PNPM_HOME="$XDG_DATA_HOME/.local/share/pnpm"
 case ":$PATH:" in
