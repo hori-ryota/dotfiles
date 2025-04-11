dd7cc42c24cd449c34e54e55199536d617b71165
hori-ryota <hori.ryota@gmail.com>
2025-04-11 20:56:37 +0900

    remove render-markdown

--- nvim/init.lua
+++ nvim/init.lua
@@ -864,14 +864,6 @@ require('lazy').setup({
           },
         },
       },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
     },
     opts = {
       behavior = {
