0dac39e7ca5f10a7dc77a52462063882193a8685
hori-ryota <hori.ryota@gmail.com>
2025-04-27 18:23:00 +0900

    fix idomatic_version_file for mise

--- zsh/modules/1-mise/install.sh
+++ zsh/modules/1-mise/install.sh
@@ -1,2 +1,7 @@
 brew install mise
 source "$(dirname "${BASH_SOURCE:-$0}")/export.zsh"
+
+mise settings add idiomatic_version_file_enable_tools go
+mise settings add idiomatic_version_file_enable_tools node
+mise settings add idiomatic_version_file_enable_tools python
+mise settings add idiomatic_version_file_enable_tools ruby
