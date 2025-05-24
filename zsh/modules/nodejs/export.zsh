6f1a9bc6d76def8faf3fb56a94ee83829f5e08cb
hori-ryota <hori.ryota@gmail.com>
2025-05-24 11:17:48 +0900

    bump libs

--- zsh/modules/nodejs/export.zsh
+++ zsh/modules/nodejs/export.zsh
@@ -1,4 +1,4 @@
-export NODE_VERSION=22.15.1
+export NODE_VERSION=22.16.0
 
 export PNPM_HOME="$XDG_DATA_HOME/.local/share/pnpm"
 case ":$PATH:" in
--- zsh/modules/python/export.zsh
+++ zsh/modules/python/export.zsh
@@ -1,4 +1,4 @@
 export PYTHON3_VERSION=3.13.3
-export UV_VERSION=0.7.5
+export UV_VERSION=0.7.7
 
 export PATH=$HOME/.local/bin:$PATH
