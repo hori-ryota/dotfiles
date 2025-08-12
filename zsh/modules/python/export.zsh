4362db8434b322c11318d681d67eaec1a3792f28
hori-ryota <hori.ryota@gmail.com>
2025-08-12 15:23:37 +0900

    bump libs

--- zsh/modules/python/export.zsh
+++ zsh/modules/python/export.zsh
@@ -1,4 +1,4 @@
-export PYTHON3_VERSION=3.13.5
-export UV_VERSION=0.8.4
+export PYTHON3_VERSION=3.13.6
+export UV_VERSION=0.8.8
 
 export PATH=$HOME/.local/bin:$PATH
--- zsh/modules/rust/export.zsh
+++ zsh/modules/rust/export.zsh
@@ -1,1 +1,1 @@
-export RUST_VERSION=1.88.0
+export RUST_VERSION=1.89.0
