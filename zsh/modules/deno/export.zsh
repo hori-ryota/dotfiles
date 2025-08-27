6ab5303f4153375d4d8a1b4f6406807a867a9d2c
hori-ryota <hori.ryota@gmail.com>
2025-08-27 12:28:23 +0900

    update libs

--- zsh/modules/deno/export.zsh
+++ zsh/modules/deno/export.zsh
@@ -1,1 +1,1 @@
-export DENO_VERSION=2.4.3
+export DENO_VERSION=2.4.5
--- zsh/modules/python/export.zsh
+++ zsh/modules/python/export.zsh
@@ -1,4 +1,4 @@
-export PYTHON3_VERSION=3.13.6
-export UV_VERSION=0.8.8
+export PYTHON3_VERSION=3.13.7
+export UV_VERSION=0.8.13
 
 export PATH=$HOME/.local/bin:$PATH
