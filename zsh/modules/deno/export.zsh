331796d15aa06d842b1447a52e4b130664a2e192
hori-ryota <hori.ryota@gmail.com>
2026-04-10 18:16:24 +0900

    bump tool versions: Deno 2.7.12, Go 1.25.9, Python 3.13.13, uv 0.11.6

--- zsh/modules/deno/export.zsh
+++ zsh/modules/deno/export.zsh
@@ -1,1 +1,1 @@
-export DENO_VERSION=2.7.11
+export DENO_VERSION=2.7.12
--- zsh/modules/python/export.zsh
+++ zsh/modules/python/export.zsh
@@ -1,4 +1,4 @@
-export PYTHON3_VERSION=3.13.12
-export UV_VERSION=0.11.3
+export PYTHON3_VERSION=3.13.13
+export UV_VERSION=0.11.6
 
 export PATH="$HOME/.local/bin:$PATH"
